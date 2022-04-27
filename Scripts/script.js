98d47758d7e4873b6c3b8dfb1b7f589a6ba0ccda
Josafat Ochoa <existense@gmail.com>
2022-04-28 01:46:51 +0200

    Commit general

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+
+const buttonHamburger = document.getElementById("buttonHamburger")
+const sidebar = document.getElementById("sidebar")
+
+buttonHamburger.addEventListener('click',()=>{
+
+   sidebar.classList.toggle('sidebar-hide')
+
+})
